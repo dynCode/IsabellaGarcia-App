0db7612399ec425334e1d3dab710830dd8254b5c
Dynamic Coders <42337291+dynCode@users.noreply.github.com>
2019-10-03 17:05:05 +0200

    Cart Functions and pages

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -13,14 +13,28 @@ export class CartPage implements OnInit {
 
   cartItems: any[] = [];
   total: any;
+  showEmptyCartMessage: boolean = false;
 
   constructor(public productsService: ProductsService, public authenticationService: AuthenticationService, private storage: Storage) { 
+
+    this.total = 0.0;
 
     this.storage.ready().then( (data)=>{
 
       this.storage.get("cart").then( (data)=>{
         this.cartItems = data;
         console.log(this.cartItems);
+
+        if (this.cartItems.length > 0) {
+
+          this.cartItems.forEach( (item, index)=>{
+            this.total = this.total + (item.product.price * item.qty)
+          })
+
+        } else {
+          this.showEmptyCartMessage = true;
+        }
+
       })
 
     })
@@ -28,6 +42,30 @@ export class CartPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  public replaceIMG(imgString) {
+    var newstr = imgString.replace("beta.", "");
+    return '<img src="' + newstr + '">';
+  }
+
+  removeFromCart(item, i) {
+
+    let price = item.product.price;
+    let qty = item.qty;
+
+    this.cartItems.splice(i, 1);
+
+    this.storage.set("cart", this.cartItems).then( ()=> {
+
+      this.total = this.total - (price * qty);
+
+    });
+
+    if (this.cartItems.length == 0) {
+      this.showEmptyCartMessage = true;
+    }
+
   }
 
 }
